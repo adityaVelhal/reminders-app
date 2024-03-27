3b261b0b3ef7fd1ddb74f7b52e2086f8ef0dafe4
adityaVelhal <aditya.v.velhal@gmail.com>
2024-03-27 23:53:27 +0530

    Reminder app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReminderList from './components/ReminderList';
+import Reminder from './models/Reminder';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [reminders, setReminders] = useState<Reminder[]>([{id:1, title: "Reminder 1"}])
+
+    return (
+        <div className="App">
+            <ReminderList items={reminders}/>
+        </div>
+    );
 }
 
 export default App;
